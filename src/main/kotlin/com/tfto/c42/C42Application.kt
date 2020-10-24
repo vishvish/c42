33f66082a750a6f6cca1e17fff9bb5d50f1ab27b
Vish Vishvanath <184423+vishvish@users.noreply.github.com>
2020-10-25 00:07:55 +0100

    Initial commit

---
+++ src/main/kotlin/com/tfto/c42/C42Application.kt
@@ -0,0 +1,11 @@
+package com.tfto.c42
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class C42Application
+
+fun main(args: Array<String>) {
+	runApplication<C42Application>(*args)
+}
---
+++ src/test/kotlin/com/tfto/c42/C42ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tfto.c42
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class C42ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
